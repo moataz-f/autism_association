d6ba5e35dd99e6fe812c1968813f57f505c546c3
iyedM <iyed.mohamed0103@gmail.com>
2026-01-28 21:56:37 +0100

    Add Modify User

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\UserProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/profile')]
+#[IsGranted('ROLE_USER')]
+class ProfileController extends AbstractController
+{
+    #[Route('/edit', name: 'app_profile_edit', methods: ['GET', 'POST'])]
+    public function edit(
+        Request $request, 
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UserProfileType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre profil a été mis à jour avec succès.');
+
+            return $this->redirectToRoute('app_profile_edit');
+        }
+
+        return $this->render('profile/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
